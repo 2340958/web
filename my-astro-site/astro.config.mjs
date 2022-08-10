38524ff9c0abca7fa722d07bb8ccf69476a308de
ram <ra@conx.ch>
2022-08-10 10:07:31 +0200

    change site and base url

--- my-astro-site/astro.config.mjs
+++ my-astro-site/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
-site = 'https://2340958.github.io/'
-base = '/web'
+site : 'https://2340958.github.io/'
+base : '/web'
